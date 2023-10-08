11785846264a78ba62a2ca441057e2cdb239e867
denisveselov <veselov.denis@gmail.com>
2023-10-09 00:23:59 +0400

    [Changed] App

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const server = http.createServer((req, res) => {
     if (parsedUrl.pathname === '/') {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello, this is the main route!');
-    } else if (parsedUrl.pathname === '/another-route') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome to another route!');
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found');
     }
 });
 
