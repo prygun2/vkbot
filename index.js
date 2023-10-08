8ce4796d1210779053ad5d76093967c3749d8cc4
denisveselov <veselov.denis@gmail.com>
2023-10-09 00:36:04 +0400

    [Changed] App

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
 
     if (parsedUrl.pathname === '/test') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'application/json'});
         res.end('Hello, this is the main route!');
     }
 });
 
-const PORT = 3333;
+const PORT = 80;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
