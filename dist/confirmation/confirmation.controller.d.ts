2f560759bd2527edac1c16121bd0a842ddf9174e
denisveselov <veselov.denis@gmail.com>
2023-10-09 03:16:35 +0400

    [Added] Reformat code

--- dist/confirmation/confirmation.controller.d.ts
+++ dist/confirmation/confirmation.controller.d.ts
@@ -2,5 +2,5 @@ import { ConfigService } from "@nestjs/config";
 export declare class ConfirmationController {
     private configService;
     constructor(configService: ConfigService);
-    createPost(body: any): string;
+    get(body: any): string;
 }
