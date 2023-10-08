730cecfc0e41c993814582a56ecbbc9bd27c2840
denisveselov <veselov.denis@gmail.com>
2023-10-09 03:24:52 +0400

    [Changed]

--- dist/confirmation/confirmation.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from "@nestjs/config";
-export declare class ConfirmationController {
-    private configService;
-    constructor(configService: ConfigService);
-    get(body: any): string;
-}
