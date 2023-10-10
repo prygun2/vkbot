8532be9151e89430e934114ca69f832dc590d33c
denisveselov <veselov.denis@gmail.com>
2023-10-10 23:34:56 +0400

    [Added] Simple API test endpoint

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/api/test') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'OK' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'NOT OK' }));
+  }
+});
+
+const PORT = 80;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
