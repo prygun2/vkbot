6ce988705bf8e4c2335ba46bf8d78a26ea680c4e
denisveselov <veselov.denis@gmail.com>
2023-10-10 23:21:06 +0400

    commit

--- app/Handlers/ConfirmHandler.ts
+++
@@ -1,9 +0,0 @@
-import Env from "@ioc:Adonis/Core/Env"
-
-export default class ConfirmHandler {
-  public async run(data: any) {
-    if (data.group_id === Env.get("GROUP_ID")) {
-      return Env.get("CONFIRMATION_CODE")
-    }
-  }
-}
